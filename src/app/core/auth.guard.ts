a21b5b5994cd106352ad1b580007357780fb7d47
pavell00 <mymmo88@gmail.comm>
2020-01-03 16:20:40 +0300

    fix auth access denided

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, 
   RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable,of } from 'rxjs';
 import { tap, map, take,switchMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
@@ -9,34 +9,37 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  isLogged: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {
-    this.authService.user$.subscribe(
-      user => {
-        if (user) {
-          this.isLogged = true;
-        } else {
-          this.isLogged = false;
-        }
-      })
-  }
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> 
-    | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot): Observable<boolean> 
+    | Promise<boolean> | boolean  {
 
-    return this.checkLogin();
+      if (this.authService.isLoggedIn) {
+        return true;
+      } else {
+        // Navigate to the login page with extras
+        console.log('Access denided!', this.authService.isLoggedIn);
+        this.router.navigate(['/welcome']);
+        return false;
+      }
+
+    //return this.authService.isLoggedIn
+    //return this.checkLogin();
     // return this.isLogged;
   }
   
-  checkLogin(): boolean {
-    // return this.isLogged;
-     if (this.isLogged) { return true; }
-    // Navigate to the login page with extras
-    console.log('Access denided!');
-    this.router.navigate(['/welcome']);
-     return false;
-  }
+  // checkLogin(): boolean {
+  //   // return this.isLogged;
+  //   if (this.isLogged) {
+  //     return true;
+  //   } else {
+  //     // Navigate to the login page with extras
+  //     console.log('Access denided!', this.isLogged);
+  //     this.router.navigate(['/welcome']);
+  //     return false;
+  //   }
+  // }
 }
--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
             
   }
 
-  ///// Login/Signup //////
+    ///// Login/Signup //////
 
   googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider()
