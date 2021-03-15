d940ca7b2cd69fc2a040891ab785ea125d970ae9
pavell00 <mymmo88@gmail.com>
2021-03-15 17:26:39 +0300

    fix split pipe

--- src/app/pipes/split-email.pipe.ts
+++ src/app/pipes/split-email.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SplitEmailPipe implements PipeTransform {
 
   transform(testEmail: string, by: string): string {
-    let pos: number = testEmail.indexOf('@') | 0;
+    let pos: number = testEmail.indexOf(by) | 0;
     let str1: string = testEmail.substr(0, pos);
     let str2: string = testEmail.substr(pos, testEmail.length);
     return str1 + ' ' + str2;
